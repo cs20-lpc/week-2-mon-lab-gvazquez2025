c0cd0fbf8ba0e0ce235a1133fa2618dbd7772164
gvazquez2025 <gianni.vazquez11@gmail.com>
2025-10-01 21:38:06 -0700

    new file:   student.cpp

---
+++ student.cpp
@@ -0,0 +1,37 @@
+#include "Student.hpp"
+#include <iostream>
+using namespace std;
+
+// Default constructor
+Student::Student() {
+    name = new string("");
+    age = new int(0);
+    cout << "Student object created!" << endl;
+}
+
+// Destructor
+Student::~Student() {
+    delete name;
+    delete age;
+    cout << "Student object destroyed!" << endl;
+}
+
+// Setter for name
+void Student::setName(string s) {
+    *name = s;
+}
+
+// Setter for age
+void Student::setAge(int i) {
+    *age = i;
+}
+
+// Getter for name
+string Student::getName() const {
+    return *name;
+}
+
+// Getter for age
+int Student::getAge() const {
+    return *age;
+}
